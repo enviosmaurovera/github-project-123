f710a4f661e3199c98877e30cd09cc1cc6158c1a
enviosmaurovera <enviosmaurovera@gmail.com>
2025-03-06 22:53:25 +0000

    Add 2.java

---
+++ src/2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class RandomJavaCode {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+
+        System.out.println("The list is: " + list);
+    }
+}
