74d7f4c64436a6b800f515373253838da673d2e7
enviosmaurovera <enviosmaurovera@gmail.com>
2025-03-17 18:23:41 +0000

    Add 13.java

---
+++ src/13.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int number = rand.nextInt(100);
+        System.out.println("The random number is: " + number);
+    }
+}
