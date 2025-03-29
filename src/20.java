4171ccba4bdc9130c5ad10336a3ba6b09a7f820e
enviosmaurovera <enviosmaurovera@gmail.com>
2025-03-29 20:10:40 +0000

    Add 20.java

---
+++ src/20.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Hello, World!");
+        
+        // Please add your code here.
+        
+        scanner.close();
+    }
+}
