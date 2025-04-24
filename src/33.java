6e2ced9a6857cb43ab2c8fb736d970b3b6b6287c
enviosmaurovera <enviosmaurovera@gmail.com>
2025-04-24 16:26:00 +0000

    Add 33.java

---
+++ src/33.java
@@ -0,0 +1,6 @@
+public class MyFirstGitHubProject {
+    public static void main(String[] args) {
+        // Example of using Java 8 features or lambda expressions in a simple example
+        System.out.println("Hello, World!");
+    }
+}
